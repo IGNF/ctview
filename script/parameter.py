8bf3f59bdb691bc77efcddd8ac97d5a6066da811
elucon <elliot.lucon@ign.fr>
2023-01-16 10:11:52 +0100

    Quel nuage de points en entrée de execute_startin ?

--- script/parameter.py
+++ script/parameter.py
@@ -2,8 +2,9 @@
 dico_param = {
     "EPSG" : 2154,
     "raster_extension" : ".tif"
+    "resolution_MNS" : 0.5,
+    "resolution_MNT" : 0.5
 }
 
-dico_folder = {
-    "folder_intensity" : "INTENSITY"
-}
+# EPSG : projection system
+# resolution_MNS : in meter
