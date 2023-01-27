6eb2aa65a8ea95de4e53f509cdda383ed81c6113
elucon <elliot.lucon@ign.fr>
2023-01-27 10:13:15 +0100

    map density branchée au reste dans main. Reste pb taille raster pour multiplier.

--- script/main.py
+++ script/main.py
@@ -91,29 +91,29 @@ def main():
             input_las=os.path.join(in_dir, f),
             output_dir=out_dir
         )
-        # Step 3 : multiply density and DTM layers
-        map_density.multiply_DTM_density(
-            input_DTM=raster_DTM_dens, 
-            input_dens_raster=raster_dens, 
-            filename=f,
-            output_dir=out_dir
+        # # Step 3 : multiply density and DTM layers
+        # map_density.multiply_DTM_density(
+        #     input_DTM=raster_DTM_dens, 
+        #     input_dens_raster=raster_dens, 
+        #     filename=f,
+        #     output_dir=out_dir
+        # )
+        # DTM hillshade color
+        map_DTM_DSM.create_map_one_las(
+            input_las=os.path.join(in_dir, f),
+            output_dir=out_dir,
+            interpMETHOD=interp_Method,
+            list_c=list_cycles,
+            type_raster="DTM"
         )
-        # # DTM hillshade color
-        # map_DTM_DSM.create_map_one_las(
-        #     input_las=os.path.join(in_dir, f),
-        #     output_dir=out_dir,
-        #     interpMETHOD=interp_Method,
-        #     list_c=list_cycles,
-        #     type_raster="DTM"
-        # )
-        # # DSM hillshade
-        # map_DTM_DSM.create_map_one_las(
-        #     input_las=os.path.join(in_dir, f),
-        #     output_dir=out_dir,
-        #     interpMETHOD=interp_Method,
-        #     list_c=list_cycles,
-        #     type_raster="DSM"
-        # )
+        # DSM hillshade
+        map_DTM_DSM.create_map_one_las(
+            input_las=os.path.join(in_dir, f),
+            output_dir=out_dir,
+            interpMETHOD=interp_Method,
+            list_c=list_cycles,
+            type_raster="DSM"
+        )
 
 
 if __name__ == "__main__":
