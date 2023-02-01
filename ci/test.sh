09b200a2e4e2830d37c70f92d4d7a049547bb3f4
elucon <elliot.lucon@ign.fr>
2023-02-01 16:09:34 +0100

    Un seul appel à pytest. Ignore DeprecationWarning.

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,1 @@
-# python -m pytest -s "test/test_utils_folder.py" -v
-# python -m pytest -s "test/test_utils_gdal.py" -v
-# python -m pytest -s "test/test_utils_pdal.py" -v
-# python -m pytest -s "test/test_map_DTM_DSM.py" -v
-# python -m pytest -s "test/test_clip_raster.py" -v
-python -m pytest -s "test/" -v
+python -m pytest -s ./test -v -W ignore::DeprecationWarning
