577a2a9a6d7b7de823178554a976b3d183c4a94d
elucon <elliot.lucon@ign.fr>
2023-02-09 17:09:50 +0100

    Correction test_get_las_liste

--- test/test_main.py
+++ test/test_main.py
@@ -7,9 +7,11 @@ IN_LAS = os.path.join(DIR_LAS,"numero1.las")
 ALL_LAS = ["numero2.las","numero1.las"]
 
 def test_get_las_liste_LAS():
-    las_liste = get_las_liste(input_las=IN_LAS, input_dir=None)
+    las_liste, in_dir = get_las_liste(input_las=IN_LAS, input_dir=None)
     assert las_liste == ["numero1.las"]
+    assert in_dir == DIR_LAS
 
 def test_get_las_liste_DIR():
-    las_liste = get_las_liste(input_las=None, input_dir=DIR_LAS)
+    las_liste, in_dir = get_las_liste(input_las=None, input_dir=DIR_LAS)
     assert set(las_liste) == set(ALL_LAS)
+    assert in_dir == DIR_LAS
