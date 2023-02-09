03e98fa1e6ad8ae7b54da0b934c29b52e6d8b0b5
elucon <elliot.lucon@ign.fr>
2023-02-09 12:08:36 +0100

    Add test for get_las_liste

---
+++ test/test_main.py
@@ -0,0 +1,15 @@
+from ctview.main import get_las_liste
+import os
+import pytest
+
+DIR_LAS = os.path.join("data","las","2_LAS")
+IN_LAS = os.path.join(DIR_LAS,"numero1.las")
+ALL_LAS = ["numero2.las","numero1.las"]
+
+def test_get_las_liste_LAS():
+    las_liste = get_las_liste(input_las=IN_LAS, input_dir=None)
+    assert las_liste == ["numero1.las"]
+
+def test_get_las_liste_DIR():
+    las_liste = get_las_liste(input_las=None, input_dir=DIR_LAS)
+    assert las_liste == ALL_LAS
