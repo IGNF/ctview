293acab9ddf02fc58579b517c0a3c89c6e83eac3
elucon <elliot.lucon@ign.fr>
2023-02-10 11:13:03 +0100

    Add test

---
+++ test/test_map_class.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+import ctview.utils_pdal as utils_pdal
+import shutil
+
+from ctview.map_class import step1_create_raster_brut, step2_create_raster_fillgap
+
+# Test files
+TEST_DIR = os.path.join("data","labo")
+DATA = os.path.join("data","las", "test_data_0000_0000_LA93_IGN69_ground.las")
+IN_POINTS = utils_pdal.read_las_file(DATA) # tested 
+FILENAME = "toto"
+
+
+# Attempted
+PATH1_EXPECTED = os.path.join(TEST_DIR,"toto_raster.tif")
+PATH2_EXPECTED = os.path.join(TEST_DIR,"toto_raster_fillgap.tif")
+
+
+def setUpModule(): # run before the first test
+    try : # Clean folder test if exists
+        shutil.rmtree(TEST_DIR)
+    except (FileNotFoundError):
+        pass
+    # Create folder test if not exists
+    os.makedirs(TEST_DIR)
+
+
+def tearDownModule(): # run after the last test
+    try : # Clean folder test if exists
+        shutil.rmtree(TEST_DIR)
+    except (FileNotFoundError):
+        pass
+
+
+def test_step1_create_raster_brut():
+    raster = step1_create_raster_brut(in_points=IN_POINTS,output_dir=TEST_DIR, filename=FILENAME, res=1)
+    assert raster == PATH1_EXPECTED # good output filename
+    assert os.path.exists(PATH1_EXPECTED) # file exists
+
+
+def test_step2_create_raster_fillgap():
+    raster_brut = step1_create_raster_brut(in_points=IN_POINTS,output_dir=TEST_DIR, filename=FILENAME, res=1)
+    raster_fillgap = step2_create_raster_fillgap(in_raster=raster_brut, output_dir=TEST_DIR, filename=FILENAME)
+    assert raster_fillgap == PATH2_EXPECTED # good output filename
+    assert os.path.exists(PATH2_EXPECTED) # file exists
+
