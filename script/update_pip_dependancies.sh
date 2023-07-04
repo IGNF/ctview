8d7e371261216afea80f4f8e8eaab89a69bba2b8
elucon <elliot.lucon@ign.fr>
2023-07-04 11:25:58 +0200

    correction path

--- script/update_pip_dependancies.sh
+++ script/update_pip_dependancies.sh
@@ -3,4 +3,4 @@ source script/get_conda.sh
 conda activate ctview
 
 # re install pip dependancies, force the download of the libs, with our private repo (Nexus).
-pip install -U --force-reinstall -r requirements.txt --extra-index-url=https://nexus.ign.fr/repository/pypi-lidarhd-hosted/simple
+pip install -U --force-reinstall -r ./../requirements.txt --extra-index-url=https://nexus.ign.fr/repository/pypi-lidarhd-hosted/simple
